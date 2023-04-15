ac645ead0db61b732efd4d456c6889d216e98e95
aidanc2004 <67815343+aidanc2004@users.noreply.github.com>
2023-04-14 22:02:03 -0300

    Allow clients to change their name with "/nick"

--- server/server.js
+++ server/server.js
@@ -1,9 +1,28 @@
 import { log } from "console";
+import { create } from "domain";
 import {WebSocket, WebSocketServer} from "ws";
 
 const wss = new WebSocketServer({port: 8080});
 
 let clients = []
+
+// create a client object
+function createClient(socket) {
+    return {
+        socket,
+        nick: "Nick",
+    };
+}
+
+// return the index of a specific client
+// because this can change, it can't just be a local variable
+function indexOfClient(socket) {
+    for (let i = 0; i < clients.length; i++) {
+        if (clients[i].socket === socket) {
+            return i;
+        }
+    }
+}
 
 wss.on('listening', () => {
     console.log("Listening for connections...");
@@ -11,15 +30,32 @@ wss.on('listening', () => {
 
 wss.on('connection', (ws) => {
     console.log("Got connection");
-    clients.push(ws);
+
+    clients.push(createClient(ws));
 
     // messages are sent as json
-    ws.send(JSON.stringify("Hello! :)"));
+    ws.send(JSON.stringify("<Server> Welcome! :)"));
 
     ws.on('message', (msg) => {
         msg = ""+msg; // convert from buffer to string
+
+        // if the client is trying to set their nickname
+        if (msg.startsWith("/nick")) {
+            // remove "/nick" from msg
+            let nick = msg.split(" "); 
+            nick.shift();
+            nick = nick.join();
+
+            // set nickname of the client
+            clients[indexOfClient(ws)].nick = nick;
+
+            return;
+        }
+
         console.log("Recieved message:", msg);
-        clients.forEach(s => s.send(JSON.stringify(msg)))
+
+        let nick = clients[indexOfClient(ws)].nick;
+        clients.forEach(c => c.socket.send(JSON.stringify(`<${nick}> ${msg}`)))
     })
     
     ws.on("close", () => {
