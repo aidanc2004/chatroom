26aa6a530e64722563191860cc092766ee476eac
aidanc2004 <67815343+aidanc2004@users.noreply.github.com>
2023-04-15 21:29:25 -0300

    Add simple css

--- client/script.js
+++ client/script.js
@@ -1,24 +1,34 @@
 const ws = new WebSocket("ws://localhost:8080");
 
+const inputForm = document.getElementById("input");
 const messages = document.getElementById("messages");
 const button = document.getElementById("button");
 const msg = document.getElementById("msg");
+
+inputForm.addEventListener("submit", (e) => {
+    e.preventDefault(); // make sending a message not refresh the page
+    msg.value = ""; // clear input field
+});
 
 ws.onopen = () => {
     console.log("Connection created");
 }
 
+// recieving a message
 ws.onmessage = (e) => {
     let msg = JSON.parse(e.data); // parse from json to string
-
-    //console.log("Got data:", msg);
 
     let msgLi = document.createElement("li");
     msgLi.innerText = `<${msg.nick}> ${msg.msg}`;
     messages.appendChild(msgLi);
 }
 
+// send a message
 button.onclick = () => {
-    //console.log("Sending:", msg.value);
+    // if trying to send an empty message
+    if (msg.value.trim() === "") {
+        return;
+    }
+
     ws.send(msg.value);
 }
