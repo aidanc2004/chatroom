c995a95b147d1ff50d9f9bcd1dc874b2d8ec35d5
aidanc2004 <67815343+aidanc2004@users.noreply.github.com>
2023-04-25 00:55:27 -0300

    Blur background when in settings

--- client/settings.js
+++ client/settings.js
@@ -5,15 +5,23 @@ const settingsBack = document.getElementById("settingsBack");
 const selectColor = document.getElementById('selectColor');
 const updateColor = document.getElementById('updateColor');
 
+const body = document.getElementById("body");
+
 settings.addEventListener("submit", (e) => e.preventDefault());
 
 settingsButton.onclick = () => {
-    main.style.display = "none";
+    main.style.filter = "blur(0.1rem)";
+    button.setAttribute("disabled", "disabled");
+    settingsButton.setAttribute("disabled", "disabled");
+
     settings.style.display = "block";
 }
 
 settingsBack.onclick = () => {
-    main.style.display = "block";
+    main.style.filter = "none";
+    button.removeAttribute("disabled");
+    settingsButton.removeAttribute("disabled");
+
     settings.style.display = "none";
 }
 
